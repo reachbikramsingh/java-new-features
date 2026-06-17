77b71e3095875ebc5ae22d6ca1de0a353cc81c61
Bikram singh <reachbikramsingh@example.com>
2026-06-17 13:56:01 +0530

    Updating test file

---
+++ src/com/bikram/collectionconceptlist/Test.java
@@ -0,0 +1,21 @@
+package com.bikram.collectionconceptlist;
+
+import java.util.ArrayList;
+
+public class Test {
+	
+	    
+	    public static void main(String args[]){
+	      
+	        // Creating an Array of string type
+	        ArrayList<String> al = new ArrayList<>();
+
+	        // 1. Adding elements to ArrayList at the end
+	        al.add("bikram");
+	        al.add("Singh");
+
+	      	System.out.println("Original List : "+al);
+	      	
+	    }
+	}
+
